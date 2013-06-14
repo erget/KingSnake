6a45d712c4b608cc22e095b1faa1cc60fe5aeaa4
Daniel Lee <lee@isi-solutions.org>
2013-06-14 08:04:03 +0200

    Group all chess errors under superclass

--- king_snake/errors.py
+++ king_snake/errors.py
@@ -1,25 +1,29 @@
 """Errors for chess game"""
 
 
-class IllegalMoveError(Exception):
+class ChessError(Exception):
+    """An error related to chess."""
+
+
+class IllegalMoveError(ChessError):
     """Proposed move is illegal."""
 
 
-class IllegalCaptureError(Exception):
+class IllegalCaptureError(ChessError):
     """Proposed capture is illegal."""
 
 
-class TurnError(Exception):
+class TurnError(ChessError):
     """Player tried to play out of order."""
 
 
-class FieldOccupiedError(Exception):
+class FieldOccupiedError(ChessError):
     """Goal field is occupied."""
 
 
-class FieldMustBeCastledError(Exception):
+class FieldMustBeCastledError(ChessError):
     """King is being moved and the field is only available for castling."""
 
 
-class PawnMustCaptureError(Exception):
+class PawnMustCaptureError(ChessError):
     """A Pawn has to capture when moving diagonally."""
